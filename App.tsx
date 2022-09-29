09d6df1ba16b69f32c6a7ccb0e69d8a67d0b354c
olashina201 <olashina201@gmail.com>
2022-09-29 19:08:42 +0100

    update codebase

--- App.tsx
+++ App.tsx
@@ -3,24 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetails from "./app/screens/ListingDetails";
 import ViewImage from "./app/screens/ViewImage";
 import Welcome from "./app/screens/Welcome";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red Jacket for sale"
-        subTitle="$100"
-        image={require("./assets/chair.jpg")}
-      />
-    </View>
+    <ListingDetails />
   );
 }
 
