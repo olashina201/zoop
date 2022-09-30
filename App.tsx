dd924d3c4014765739e3c787c499772ff5201331
olashina201 <olashina201@gmail.com>
2022-09-30 15:38:16 +0100

    update codebase

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetails from "./app/screens/ListingDetails";
+import Messages from "./app/screens/Messages";
 import ViewImage from "./app/screens/ViewImage";
 import Welcome from "./app/screens/Welcome";
 
 export default function App() {
   return (
-    <ViewImage />
+    <Messages />
   );
 }
 
