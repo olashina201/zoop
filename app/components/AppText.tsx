86bcb62aa83c64cb9769b0315a4311a49cbe43e1
olashina201 <olashina201@gmail.com>
2022-10-03 11:31:23 +0100

    update codebase

--- app/components/AppText.tsx
+++ app/components/AppText.tsx
@@ -3,8 +3,12 @@ import { Text } from "react-native";
 
 import defaultStyles from "../config/styles";
 
-const AppText = ({ children, style }: any) => {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+const AppText = ({ children, style, ...otherProps }: any) => {
+  return (
+    <Text {...otherProps} style={[defaultStyles.text, style]}>
+      {children}
+    </Text>
+  );
 };
 
 export default AppText;
