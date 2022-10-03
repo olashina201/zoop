148893ead0ebd9e8b853fe5a5077140ebe1a0519
olashina201 <olashina201@gmail.com>
2022-10-03 08:44:48 +0100

    update codebse

--- app/components/AppPicker.tsx
+++ app/components/AppPicker.tsx
@@ -1,27 +1,46 @@
-import React from "react";
-import { TextInput, View, StyleSheet, Platform } from "react-native";
+import React, { useState } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
+import Screen from "./Screen";
 
 const AppPicker = ({ icon, placeholder, ...otherProps }: any) => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          name={icon}
-          size={20}
-          color={defaultStyles.colors.medium}
-          style={styles.icon}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons
-          name="chevron-down"
-          size={20}
-          color={defaultStyles.colors.medium}
-        />
-    </View>
+    <React.Fragment>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </React.Fragment>
   );
 };
 
@@ -39,7 +58,7 @@ const styles = StyleSheet.create({
   },
   text: {
     flex: 1,
-  }
+  },
 });
 
 export default AppPicker;
