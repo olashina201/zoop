627b8e6ed94c369bb226343ab06ef323a0aca006
olashina201 <olashina201@gmail.com>
2022-11-21 21:32:28 +0100

    update codebase

--- app/components/ImageInput.tsx
+++ app/components/ImageInput.tsx
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 import React, { Component } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
@@ -7,6 +7,7 @@ export default function ImageInput({ imageUri }: any) {
   return (
     <View style={styles.container}>
       {!imageUri && <MaterialCommunityIcons name="camera" color={colors.medium} size={40} />}
+      {imageUri && <Image source={{ uri: imageUri}} style={styles.image} />}
     </View>
   );
 }
@@ -17,7 +18,14 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     justifyContent: "center",
     alignItems: "center",
+    overflow: "hidden",
     height: 100,
     width: 100,
   },
+  image: {
+    width: "100%",
+    height: "100%",
+;
+    height: 100%;
+  }
 });
