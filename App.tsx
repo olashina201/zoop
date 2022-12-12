4d0e5962bed8a998e2aff62f21932e7ad2969d94
olashina201 <olashina201@gmail.com>
2022-12-12 23:45:08 +0100

    stach navigation

--- App.tsx
+++ App.tsx
@@ -1,42 +1,34 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import AppButton from "./app/components/AppButton";
-import AppPicker from "./app/components/AppPicker";
-import AppText from "./app/components/AppText";
-import AppTextInput from "./app/components/AppTextInput";
+import { Text } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Screen from "./app/components/Screen";
-import Login from "./app/screens/Login";
 import ListingEdit from "./app/screens/ListingEdit";
-import Listings from "./app/screens/Listing";
-import Account from "./app/screens/Account";
-import ListingDetails from "./app/screens/ListingDetails";
-import Messages from "./app/screens/Messages";
-import Register from "./app/screens/Register";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
 
-// const categories = [
-//   { label: "Furniture", value: 1 },
-//   { label: "Furniture", value: 2 },
-//   { label: "Furniture", value: 3 },
-// ];
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+)
 
 export default function App() {
-  const [imageUris, setImageUris] = useState<any>([]);
-
-  const handleAdd = (uri: any) => {
-    setImageUris([...imageUris, uri]);
-  };
-  
-  const handleRemove = (uri: any) => {
-    setImageUris(imageUris.filter((imageUris: any) => imageUris !== uri));
-  };
-
-  // return (
-  //   <Screen>
-  //     <ImageInputList imageUris={imageUris} onAddImage={handleAdd} onRemoveImage={handleRemove} />
-  //   </Screen>
-  // );
-  return <ListingEdit />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
