cae9f2c8423ef0e5cd3404f7af6b17dd277f8fbb
olashina201 <olashina201@gmail.com>
2022-12-13 09:13:56 +0100

    header style

--- App.tsx
+++ App.tsx
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Screen from "./app/components/Screen";
 import ListingEdit from "./app/screens/ListingEdit";
 
-const Tweets = () => (
+const Tweets = ({ navigation }: any) => (
   <Screen>
     <Text>Tweets</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
   </Screen>
-)
+);
 
 const TweetDetails = () => (
   <Screen>
     <Text>Tweet Details</Text>
   </Screen>
-)
+);
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
     <Stack.Screen name="Tweets" component={Tweets} />
     <Stack.Screen name="TweetDetails" component={TweetDetails} />
   </Stack.Navigator>
-)
+);
 
 export default function App() {
   return (
