9543a1bc4969e3dcdf16b841d820ef4cbdd1b615
olashina201 <olashina201@gmail.com>
2022-12-13 23:34:39 +0100

    activity indicator

--- app/screens/Listing.tsx
+++ app/screens/Listing.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet } from "react-native";
 import * as api from "../api/listing";
 
 import Screen from "../components/Screen";
@@ -12,6 +12,7 @@ import AppButton from "../components/AppButton";
 function Listings({ navigation }: any) {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   console.log(error)
 
@@ -20,7 +21,10 @@ function Listings({ navigation }: any) {
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await api.getListings();
+    setLoading(false);
+
     if (!response.ok) return setError(true);
 
     setError(false);
@@ -34,6 +38,7 @@ function Listings({ navigation }: any) {
           <AppButton title="Retry" onPress={loadListings} />
         </>
       )}
+      <ActivityIndicator animating={loading} size="large" />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
