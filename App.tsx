229f333a9641a8bc23f4d14a6cfedaf6a2911e67
olashina201 <olashina201@gmail.com>
2022-12-14 23:29:04 +0100

    app loading and slach screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { AppLoading } from "expo";
+
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import authStorage from "./app/auth/storage";
@@ -10,22 +12,25 @@ import jwtDecode from "jwt-decode";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
-    if(!token) return;
+    if (!token) return;
     setUser(jwtDecode(token));
-  }
+  };
 
-  useEffect(() => {
-    restoreToken();
-  }, [])
+  // useEffect(() => {
+  //   restoreToken();
+  // }, [])
+
+  if (!isReady) return <AppLoading startAsync={restoreToken} onFinish={() =>  setIsReady(true)} />;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        { user ? <AppNavigator /> : <AuthNavigator />}
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
   );
